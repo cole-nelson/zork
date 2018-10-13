b1439ef91231c86a15a4c48d5452660e7b0cc45d
ZGuo412 <43893359+ZGuo412@users.noreply.github.com>
2018-10-13 15:14:09 -0400

    Add files via upload

---
+++ src/Container.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <iostream>
+#include "../inc/container.h"
+
+container::container(string na, string st, string des : name(na), status(st), description(des) {}
+
+container::~container() {}
