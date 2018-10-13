8f89ae354ada204fecf4456ba3d818367beb03e7
ZGuo412 <43893359+ZGuo412@users.noreply.github.com>
2018-10-13 15:29:28 -0400

    Add files via upload

---
+++ src/Creature.cpp
@@ -0,0 +1,8 @@
+#include <string>
+#include <iostream>
+#include "../inc/Creature.h"
+
+Creature::Creature(string na, string st, string des : name(na), status(st), description(des) : GameObject(na, st, des){
+}
+
+Creature::~Creature() {}
