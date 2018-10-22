49cba02c2769fd34b08e669141ff0be00878274e
aclouditation <25907312+AClouDitation@users.noreply.github.com>
2018-10-21 20:11:51 -0400

    better structure for trigger

--- inc/Trigger.h
+++ inc/Trigger.h
@@ -5,27 +5,60 @@
 #include "GameObject.h"
 
 using namespace std;
+
+class Conditation{
+private:
+    GameObject* target;
+public:
+    Conditation();
+    virtual ~Conditation();
+    virtual bool checkConditation()=0;
+};
+
+class HasConditation : public Conditation{
+private:
+    bool has;
+    string hasName;
+public:
+    bool checkConditation();
+};
+
+class StatConditation : public Conditation{
+private:
+    string stat;
+public:
+    bool checkConditation();
+};
+
+
 class Trigger{
 private:
+    string cmd;
 	string type;
 	string print;
 	vector <string> action;
 	string command;
 	GameObject* owner;
+    Conditation* cond;
 	string stat;
 public:
 	Trigger();
 	Trigger(const Trigger& );
 	virtual ~Trigger();
+
 	virtual void setType(string);
 	virtual void setPrint(string);
 	virtual void setCommand(string);
 	virtual void setOwner(GameObject*);
 	virtual void setStatus(string);
-	string getType();
+
+    string getType();
 	string getPrint();
 	string getCommand();
 	GameObject* getOwner();
 	string getStatus();
+
+    void fire(string);
 };
+
 #endif
--- src/Trigger.cpp
+++ src/Trigger.cpp
@@ -1,6 +1,6 @@
 #include "../inc/Trigger.h"
 
-Trigger::Trigger() : type(""), print(""),command(""),owner(NULL),stat("") {}
+Trigger::Trigger() : cmd(""), type("permanent"), print(""),command(""),owner(NULL),stat("") {}
 Trigger::Trigger(const Trigger& trig) :
 		type(trig.type),print(trig.type),command(trig.type),owner(trig.owner),stat(trig.stat) {}
 
@@ -17,3 +17,12 @@ string Trigger::getPrint() {return print;}
 string Trigger::getCommand() {return command;}
 GameObject* Trigger::getOwner() {return owner;}
 string Trigger::getStatus() {return stat;}
+
+void Trigger::fire(string input_cmd){
+
+    if(cmd != "" && cmd != input_cmd) return; // nothing to do with this trigger
+
+    
+
+
+}
